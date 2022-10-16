f9c79bc1e1de9823e168879b6dbf1185173235d1
Evgeny Kolotov <kolotov@wiseadvice.ru>
2022-10-16 16:09:35 +0300

    Установлены основные библиотеки для приложения

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgxsModule } from '@ngxs/store';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxsSelectSnapshotModule } from '@ngxs-labs/select-snapshot';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsActionsExecutingModule } from '@ngxs-labs/actions-executing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    NgxsSelectSnapshotModule.forRoot(),
+    NgxsActionsExecutingModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
